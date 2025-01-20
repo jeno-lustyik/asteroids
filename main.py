c43ec4353c361d97fa7c311653a5a34fd960509f
jeno-lustyik <lustyik.jeno@gmail.com>
2025-01-20 12:29:43 +0100

    player sprite + window fix

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(player.position)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return
 
         screen.fill("black")
-        pygame.display.flip()
         player.draw(screen)
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
